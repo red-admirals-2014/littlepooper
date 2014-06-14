3a04ebbe25b294c66065f34cdfccb6436913d544
William Tsay <willtsay@WTSAY.local>
2014-06-14 16:52:04 -0700

    added a preloader, egg scene leads into crappy bird

--- app/assets/javascripts/gameinitializer.js
+++ app/assets/javascripts/gameinitializer.js
@@ -1,8 +1,8 @@
 (function () {
   var game = new Phaser.Game(480, 640, Phaser.AUTO, 'game_div'); 
+  game.state.add('Preloader', Scene.Preloader)
   game.state.add('HomePage', Scene.HomePage); 
-  game.state.add('Egg')
   game.state.add('Stomper', Scene.Stomper); 
   game.state.add('Egg', Scene.Egg) 
-  game.state.start('Egg'); // Go to Boot scene (start the game)
+  game.state.start('Preloader'); // Go to Boot scene (start the game)
 })();
