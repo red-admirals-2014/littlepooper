a8fde7fc3d962efacdc480b6922d79e48583d7fe
Ed Chun <e7chun@gmail.com>
2014-06-13 21:12:58 -0700

    added background image to home page

--- app/assets/javascripts/scenes/HomePage.js
+++ app/assets/javascripts/scenes/HomePage.js
@@ -1,1 +1,21 @@
-HomePage.js
+Scene.HomePage = function(game) {
+
+};
+
+Scene.HomePage.prototype = {
+	preload: function(){
+		this.game.load.image('clouds','/images/clouds.png');
+		this.game.load.image('land','/images/land.png');
+	},
+
+	create: function() {
+
+		clouds = this.game.add.tileSprite(0, 0, 640, 138, 'clouds');
+		land = this.game.add.tileSprite(0, 138, 640, 1020, 'land')
+  	},
+
+  	update: function() {
+  		clouds.tilePosition.x += 1;
+  	}
+
+};
