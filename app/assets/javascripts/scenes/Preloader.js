1a4f2fc4374c813b90e0eddd875eb348ad7614e5
Michael Ohm <michael.k.ohm@gmail.com>
2014-06-14 19:59:34 -0700

    pooper flails then rolls into main menu

--- app/assets/javascripts/scenes/Preloader.js
+++ app/assets/javascripts/scenes/Preloader.js
@@ -6,7 +6,7 @@ Scene.Preloader.prototype = {
   preload: function() {
     // this.game.stage.backgroundColor = '#dddddd'
     this.game.load.spritesheet('egg', 'images/egg_break.png', 118, 130, 6)
-    this.game.load.spritesheet('green_dragon', 'images/green_dragon.png', 110, 110, 32)
+    this.game.load.spritesheet('green_dragon', 'images/green_dragon.png', 110, 110, 40)
     this.game.load.image('bird', '/images/bird.png')
     this.game.load.image('pipe', '/images/pipe.png')
     this.game.load.image('clouds','/images/clouds.png');
