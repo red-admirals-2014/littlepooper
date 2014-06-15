06b028887230c259ab4d53208a2c4fa6b9898afb
Michael Ohm <michael.k.ohm@gmail.com>
2014-06-14 20:21:10 -0700

    added random movement

--- app/assets/javascripts/scenes/HomePage.js
+++ app/assets/javascripts/scenes/HomePage.js
@@ -4,7 +4,6 @@ Scene.HomePage = function(game) {
 
 Scene.HomePage.prototype = {
 
-
 	create: function() {
 		clouds = this.game.add.tileSprite(0, 0, 640, 138, 'clouds');
 		land = this.game.add.tileSprite(0, 138, 640, 1020, 'land')
@@ -12,9 +11,17 @@ Scene.HomePage.prototype = {
     this.green_dragon.anchor.set(0.5)
     this.green_dragon.animations.add('rest', [0, 1, 2, 3, 4, 5, 6, 7], 6, true)
     this.green_dragon.animations.play('rest') 
-  	},
+    setTimeout(this.walkAround.bind(this), 2000)
 
-  	update: function() {
+  },
+  update: function() {
   		clouds.tilePosition.x += 1;
-  	}
+  },
+  walkAround: function() {
+      var walk = this.game.add.tween(this.green_dragon)
+      walk.to({x: this.green_dragon.position.y+Math.floor(Math.random()*(300))}, 1000)
+      walk.start();
+    	setTimeout(this.walkAround.bind(this), 2000)
+  }
+
 };
