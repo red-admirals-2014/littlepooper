979226ffdc4ab946820d751be1f368ace61752af
Ed Chun <e7chun@gmail.com>
2014-06-15 17:00:22 -0700

    fixed exercise button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -36,7 +36,7 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to home_path
     else
       render :new
     end
