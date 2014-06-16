fa37f14c0ccad8e713e895abc60a5adf29b5a452
Ed Chun <e7chun@gmail.com>
2014-06-15 17:46:48 -0700

    changed website view dimensions...allowed user to create account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params_user)
-    if @user.save && @user.authenticate(params[:password])
+    if @user.save 
       session[:user_id] = @user.id
       redirect_to '/home'
     else
