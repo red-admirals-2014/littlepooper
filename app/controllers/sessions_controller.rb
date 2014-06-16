c84d74daa5dc2f526d25e5721ebb562b69db55f3
William Tsay <willtsay@WTSAY.local>
2014-06-15 22:58:11 -0700

    added google thing to heroku link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     google_url = "https://accounts.google.com/o/oauth2/auth?" +
                     "response_type=code&"+
                     "client_id=#{Rails.application.secrets.client_id}&"+
-                    "redirect_uri=http://localhost:3000/logged_in&"+
+                    "redirect_uri=http://little-pooper.herokuapp.com/logged_in&"+
                     "scope=email%20profile&"+
                     "state=#{Rails.application.secrets.state}&"
     redirect_to google_url
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
   def logged_in
     access_code = params[:code]
-    first_response = HTTParty.post("https://accounts.google.com/o/oauth2/token?", {body: {client_id: Rails.application.secrets.client_id, client_secret: Rails.application.secrets.client_secret, redirect_uri: "http://localhost:3000/logged_in", grant_type:"authorization_code", code: access_code}})
+    first_response = HTTParty.post("https://accounts.google.com/o/oauth2/token?", {body: {client_id: Rails.application.secrets.client_id, client_secret: Rails.application.secrets.client_secret, redirect_uri: "http://little-pooper.herokuapp.com/logged_in", grant_type:"authorization_code", code: access_code}})
     access_token = first_response["access_token"]
     user_info = HTTParty.get("https://www.googleapis.com/plus/v1/people/me?", {query: {access_token: access_token}})
     #check if their email is in the database, otherwise write them in. 
