49fbdc3ac68cf25b9c9c8fe11623dd169651ccfe
William Tsay <willtsay@WTSAY.local>
2014-06-16 23:04:26 -0700

    transparency work around.. works but looks like ass

--- app/assets/javascripts/scenes/FlappyDragon.js
+++ app/assets/javascripts/scenes/FlappyDragon.js
@@ -58,10 +58,12 @@ Scene.FlappyDragon.prototype = {
       this.green_dragon_fly.body.gravity.y=2000;
 
       this.game.time.events.remove(this.timer)
-      this.play_again = this.game.add.button(75, 300, "exercise_button", this.restart_game, this, 0,1,2)
-      this.return_home = this.game.add.button(250, 300, "homes_button", this.go_home, this, 0,1,2 )
-      this.high_scores = this.game.add.button(75, 100, "egg", this.showHighScores, this, 0,1,2)
+      this.updateHighScore()
+      SHOWFLAPPYOPTIONS = true
+      this.alive = true
+      this.game.state.start('HomePage')
 
+      
     },
     restart_game: function() {
       this.updateHighScore()
