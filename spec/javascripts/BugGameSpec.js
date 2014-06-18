bdac7ed7bdfe6329e288276a8d75b9344b858b83
Kenji Miwa <kenjimiwa@Kenjis-MacBook-Pro.local>
2014-06-17 19:42:34 -0700

    BugGame refactoring and some tests

--- spec/javascripts/BugGameSpec.js
+++ spec/javascripts/BugGameSpec.js
@@ -1,13 +1,36 @@
 describe("Scene.BugGame", function() {
   var bugGame, game;
   beforeEach(function() {
-    game = {}
+    game = {
+      add: {
+        text: function() {}
+      }
+    }
     bugGame = new Scene.BugGame(game);
   })
+
   it("#bugTotal is initialized with 50", function() {
     expect(bugGame.bugsTotal).toEqual(50);
   })
+
   it("#addMonster creates a new Monster instance", function() {
-    expect(bugGame.addMonster).toEqual('');
+    var monsterName = 'fake monster'
+    stubMonster(monsterName);
+    bugGame.addMonster()
+    expect(bugGame.player.name).toEqual( monsterName );
+  })
+
+  it("#addDisplays calls game.add.text function", function() {
+    var addTextReturn = 'fakeValue';
+    spyOn(game.add, 'text').and.returnValue(addTextReturn);
+    // we need to fake the spy arguments this.bugsKilled.
+    bugGame.bugsKilled = [];
+    bugGame.bugsEscaped = [];
+
+    bugGame.game = game;
+    bugGame.addDisplays();
+    expect(bugGame.game.add.text.calls.count()).toEqual(2);
+    expect(bugGame.score).toEqual(addTextReturn);
+    expect(bugGame.escaped).toEqual(addTextReturn);
   })
 })
