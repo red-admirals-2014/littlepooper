dad239be7cd1ece46f5f2e650ae2334eb985855b
William Tsay <willtsay@WTSAY.local>
2014-06-18 11:23:14 -0700

    added a loading_screen and the egg scene

--- app/assets/javascripts/gameinitializer.js
+++ app/assets/javascripts/gameinitializer.js
@@ -3,14 +3,14 @@
 
   SHOWFLAPPYOPTIONS = false
 
-
+  game.state.add('Boot', Scene.Boot)
   game.state.add('Preloader', Scene.Preloader)
   game.state.add('HomePage', Scene.HomePage);
   game.state.add('BugGame', Scene.BugGame)
   game.state.add('FlappyDragon', Scene.FlappyDragon);
   game.state.add('FlappyHighScores', Scene.FlappyHighScores)
   game.state.add('Egg', Scene.Egg)
-  game.state.start('Preloader'); // Go to Boot scene (start the game)
+  game.state.start('Boot'); // Go to Boot scene (start the game)
 })();
 
 
