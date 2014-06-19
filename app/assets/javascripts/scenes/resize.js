ce9d18f2877eb66319b8ad9ca3d9fb12f4f5b43d
Ed Chun <e7chun@gmail.com>
2014-06-19 15:19:41 -0700

    cleaned up code

--- app/assets/javascripts/scenes/resize.js
+++
@@ -1,7 +0,0 @@
-// window.addEventListener('resize', function(event){
-//     resizeGame();
-// });
-// var resizeGame = function () {
-//     game.stage.scale.setShowAll();
-//     game.stage.scale.refresh();
-// }
