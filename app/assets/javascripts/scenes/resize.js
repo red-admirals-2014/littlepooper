94dfb71eeb2dc22eae890c0a898b2513a2a1c980
Ed Chun <e7chun@gmail.com>
2014-06-19 11:15:09 -0700

    testing full screen

---
+++ app/assets/javascripts/scenes/resize.js
@@ -0,0 +1,7 @@
+window.addEventListener('resize', function(event){
+    resizeGame();
+});
+var resizeGame = function () {
+    this.game.stage.scale.setShowAll();
+    this.game.stage.scale.refresh();
+}
